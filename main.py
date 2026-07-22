4784221ae73c3dc12ce88ad6cc4ae605eb4159c3
mxzy-dev <mxzydev@gmail.com>
2026-07-22 18:21:56 +0200

    Update main.py
    Bug Fix + Clean Code -- 1.0.1

--- main.py
+++ main.py
@@ -1,96 +1,115 @@
 import os
 import requests as rq
 
-#elements
+#-----------------------elements------------------------
 
-enter = "-Нажмите Enter чтобы продолжить--}"
-CLEAR = "\033[H\033[2J"
+Enter = "-Нажмите Enter чтобы продолжить--}"
 BLUE = "\033[1;34m"
-RESET = "\033[0m"
-banner = f"""{BLUE}
-\t\t╔════════════════════════════════╗
-\t\t ║                                                                                 ║
-\t\t║        M X Z Y   A N A L I S Y S   H U B                     ║
-\t\t ║by Mxzy.                                                                         ║
-\t\t╚════════════════════════════════╝
-"""
+RESET = "\033[1;34m"
 
-#helpers functions
+bannr = f"""{BLUE}
+╔═════════════════════════════════════════════════════════════════════╗
+║                                                                     ║
+║            M X Z Y   A N A L I S Y S   H U B                        ║
+║                                                                     ║
+╚═════════════════════════════════════════════════════════════════════╝
+{RESET}"""
 
-def menu():
-      try:
-            print(banner)
-            print(CLEAR)
-            print(banner)
-            print("\t\t\t\t\t\t--ФУНКЦИИ--")
-            print("\n0. Базовый анализис веб сайтов\t\t|\t1. Выйти")
-            sel = int(input("\n\n-Выберите функцию:"))
-            if sel == 0:
-                  retry(scan)
-            elif sel == 1:
-                  quit()
-            else:
-                  print("\n-Выберите один из вариантов!")
-                  input(enter)
-                  retry(menu)
-      except TypeError:
-            print("-Вставте цифру выбранного варианта!")
-            input(enter)
-            retry(menu)
+#-------------------helper functions--------------------
 
-def retry(x):
-      print(CLEAR)
-      x()
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def banner():
+      clear()
+      print(bannr)
+      
+def retry(x,y):
+      clear()
+      x(y)
 
 def rec(num,dir, name, x):
       with open(f"{dir}{name}", "w",encoding="utf-8") as num:
             num.write(x)
 
-#analisys
+#----------------Select-----------------
 
-def urly(dir):
-      url = input("\n-Вставте ссылку для анализа:")
-      if url == "":
-            urly()
-      req = rq.get(url)
-      print(req)
-      rsq = rq.options(url)
-      print(rsq)
-      print(f"доступные методы: {rsq.headers.get('Allow')} ")
-      print("-----------------------------")
-      rec(1,dir,"url.txt",req.url)
-      rec(2,dir,"text.txt", req.text)
-      rec(3,dir,"siteoffl.html", req.text)
+def Select(Dir):
       try:
-            rec(4,dir,"json.txt",req.json())
-      except rq.exceptions.JSONDecodeError:
-            rec(4,dir,"json.txt","there is no json")
+            banner()
+            print("----ФУНКЦИИ----")
+            print("\n0. Базовый анализис сайтов\n1. SQL иньекции\n2. Выход")
+            Select = int(input("\n\n-Выберите функцию:--"))
+            if Select == 0:
+                  retry(urly,Dir)
+            elif Select == 1:
+                  retry(SQL)
+            elif Select == 2:
+                  quit()
+            else:
+                  print("\n-Выберите один из вариантов!")
+                  input(Enter)
+                  retry(Select,Dir)
+      except TypeError:
+            print("-Вставте цифру выбранного варианта!")
+            input(Enter)
+            retry(Select,Dir)
 
-#start
+#---------------analisys--------------
 
-def scan():
-      print(banner)
-      print(CLEAR)
-      print(banner)
+def urly(Dir):
       try:
-            d = input(f"{RESET}\n-Название созданной папки куда будут направленны результаты анализа:\n____________________________________________________________________\n")
-            print("-----------------------------------------------------------------------------------")
-            if d == "" :
+            url = input("\n-Вставте ссылку для анализа:")
+            if url == "":
+                  retry(urly,Dir)
+            GetRequest = rq.get(url)
+            print(GetRequest)
+            GetOptions = rq.options(url)
+            print(GetOptions)
+            print(f"доступные методы: {GetOptions.headers.get('Allow')} ")
+            print("-----------------------------")
+            rec(1,Dir,"url.txt",GetRequest.url)
+            rec(2,Dir,"text.txt", GetRequest.text)
+            rec(3,Dir,"siteoffl.html", GetRequest.text)
+      except rq.exceptions.InvalidSchema:
+            retry(urly,Dir)
+      except rq.exceptions.MissingSchema:
+            retry(urly,Dir)
+      except rq.exceptions.InvalidURL:
+            retry(urly,Dir)
+      try:
+            rec(4,Dir,"json.txt",GetRequest.json())
+      except:
+            rec(4,Dir,"json.txt","there is no json")
+      input(Enter)
+      retry(Directory,0)
+
+#-------------------------directory-----------------------
+
+def Directory(n):
+      banner()
+      try:
+            dr = input("\n-Название папки которая будет созданна и куда будут направленны результаты анализа:\n\n---")
+            dir = f"AnalisysRep/{dr}"
+            if dr == "" :
                   print("-Вы не можете указать пустоту!")
-                  retry(scan)
-            dir = f"/sdcard/{d}/"
-            with open(f"{dir}README.txt", "w",encoding="utf-8") as rdm:
-                  rdm.write("Это универсальный инструмент созданный Mxzy. dvlp . Используйте его с умом для благих целей. Данный инструммент дает доступ только к публично досигаемой информации в рамках закона.\nBy Mxzy.")
-            
-      except FileNotFoundError:
-            print("Поиск папки:", os.path.abspath(dir))
-            print("\n\n-Вы указали несуществующую или недоступную папку.")
-            input(enter)
-            retry(scan)
-      urly(dir)
-      input(enter)
-      menu()
-  
-#run
+                  retry(Directory,0)
+            os.mkdir(dir)
+            DirResult = os.path.exists(dir)
+            Dir = f"{dir}/"
+            print(DirResult)
+      except TypeError:
+            print("\n\n-Данное название не может быть использованно.")
+            input(Enter)
+            retry(Directory,0)
+      Select(Dir)
 
-menu()
+
+#--------------------------SQL-injection-------------------------
+
+def SQL(url):
+      pass
+
+#-----------------------------Run-----------------------------
+
+Directory(0)
